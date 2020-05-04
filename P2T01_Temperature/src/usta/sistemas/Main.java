ae1afb4a9cbf8c544098737b70217059a2dd2424
Santiago Hernández Molina <santiago.hernandez@usantoto.edu.co>
2020-05-03 20:15:31 -0500

    Java_Basic

---
+++ P2T01_Temperature/src/usta/sistemas/Main.java
@@ -0,0 +1,18 @@
+package usta.sistemas;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+    }
+    public static void f_menu(){
+        //This method show thw menu of the software
+        System.out.println(" ___________________________________");
+        System.out.println("|  SoftRadioAt                      |");
+        System.out.println("| Version 1.0 21-04-2020            |");
+        System.out.println("| Created by Santiago Hernández     |");
+        System.out.println("|___________________________________|");
+        System.out.println("                                     ");
+    }
+
+
+}
