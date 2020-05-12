2dceed4881375dfa851f10484cf4a047053079f9
Santiago Hernández Molina <santiago.hernandez@usantoto.edu.co>
2020-05-12 09:32:11 -0500

    Revert "Java_Basic"
    This reverts commit 70321cbd647b010be9c395a33a70cde90950a6d3.

--- P2T02_Average_Height/src/usta/sistemas/Main.java
+++ P2T02_Average_Height/src/usta/sistemas/Main.java
@@ -1,7 +1,5 @@
 package usta.sistemas;
-
 import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
@@ -9,11 +7,12 @@ public class Main {
         //DATE: 13-04-2020
         //DESCRIPTION: this software calculates the average height of a group of 5 children
         f_menu();
-        double summatory = f_sum_height();
-        System.out.println("The summatory is: " + summatory + " mts");
+        double summatory=f_sum_height();
+        System.out.println("The summatory is: "+summatory+ " mts");
         f_average(summatory);
-    }
 
+
+    }
     public static void f_menu() {
         //This method show thw menu of the software
         System.out.println(" ___________________________________");
@@ -22,23 +21,30 @@ public class Main {
         System.out.println("| Created by Santiago Hernández     |");
         System.out.println("|___________________________________|");
         System.out.println("                                     ");
+        System.out.println("input the kind of option grades: \n1.Celsius \n2.Fahrenheit \n3.kelvin");
     }
-
     public static double f_sum_height() {
         //Description: This method calculate the summatory of 5 height
         Scanner keyboard = new Scanner(System.in);
-        double summatory = 0, height;
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Input the  height" + i + " (mts):");
-            height = keyboard.nextInt();
-            summatory += height;
-        }
+        double summatory, height1, height2, height3, height4, height5;
+        System.out.println("Input the firts height (mts):");
+        height1 = keyboard.nextDouble();
+        System.out.println("Input the second height (mts):");
+        height2 = keyboard.nextDouble();
+        System.out.println("Input the third height (mts):");
+        height3 = keyboard.nextDouble();
+        System.out.println("Input the fourth height (mts):");
+        height4 = keyboard.nextDouble();
+        System.out.println("Input the five height (mts):");
+        height5 = keyboard.nextDouble();
+        summatory = height1 + height2 + height3 + height4 + height5;
         return summatory;
     }
-
-    public static void f_average(double summatory) {
+    public static void f_average(double summatory){
         //Description: This method calculate the average
-        double average = summatory / 5;
-        System.out.println("The average is: " + average + " (mts)");
+        double average= summatory/5;
+        System.out.println("The average is: "+average+ " (mts)");
     }
+
+
 }
